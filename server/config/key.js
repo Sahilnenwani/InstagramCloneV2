c28ecf5fdd36003f2918e844ea9b93994ae888a8
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-14 15:10:41 +0500

    connected a database

---
+++ server/config/key.js
@@ -0,0 +1,14 @@
+// module.exports={
+//     MONGOURI:'mongodb://localhost:27017/InstagramClonev2'
+// }
+
+// module.exports={
+//     //  MONGOURI:'mongodb://localhost:27017/InstagramClonev2'
+// }
+
+require('dotenv').config('./.env')
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 
 const app = express()
 const PORT = process.env.PORT || 5000;
 
 
+const db = require("./config/key").mongoURI;
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB Connected...!"))
+  .catch((err) => console.log(err));
+
+
+
 app.get('/',(req,res)=>{
     res.send("Server Is Running!");
 })
+
 app.listen(PORT,()=>{
     console.log(`Server is runnig http://localhost:${PORT}/`)
 })
