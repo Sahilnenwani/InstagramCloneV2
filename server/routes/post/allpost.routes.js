f9ead026fa9dc481c605052cd0c7aad96645590f
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-18 22:39:50 +0500

    creatpost post routes and allpost seen getroutes

--- server/routes/post/allpost.routes.js
+++ server/routes/post/allpost.routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
-const requiredLogin = require('../../middleware/requireLogin')
 const Post = mongoose.model("Post")
 
 router.get('/allpost',(req,res)=>{
