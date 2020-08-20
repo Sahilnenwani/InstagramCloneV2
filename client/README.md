e08b25994485e83da6588da8328383e6f40d512b
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-20 18:41:06 +0500

    add libary url materlaize link in html file and make components nav bar and screen components home,sigin,sigup, and profile make and react router dom libary use app.js and navbar.jsx

--- client/README.md
+++ client/README.md
@@ -66,3 +66,18 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `yarn build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+
+
+
+
+
+
+
+
+
+
+https://materializecss.com/
+https://material-ui-x.netlify.app/?path=/story/1-docs-2-columns--page
+https://material-ui.com/
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter,Route} from 'react-router-dom';
 import './App.css';
+import Home from "./components/Screen/Home/Home";
+import Profile from "./components/Screen/Profile/Profile";
+import Signin from "./components/Screen/Signin/Signin";
+import Signup from "./components/Screen/Signup/Signup";
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <BrowserRouter>
+    <Navbar/>
+    <Route  path='/'  exact component={Home}/>
+    <Route  path='/Signin'  component={Signin}/>
+    <Route  path='/signup'  component={Signup}/>
+    <Route  path='/profile'  component={Profile}/>
+    
+    </BrowserRouter>
   );
 }
 
