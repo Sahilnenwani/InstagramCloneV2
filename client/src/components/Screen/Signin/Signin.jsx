5a23ed976d7c0e47da8cf64557e357b56960bd1a
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-20 21:25:29 +0500

    working with two form signin form and signup form with use a css also done the job

--- client/src/components/Screen/Signin/Signin.jsx
+++ client/src/components/Screen/Signin/Signin.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 const Signin = ()=> {
     return (
-        <h1>Signin</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+            <h2>Instagram</h2>
+            <input 
+            type="email"
+            placeholder="email"
+            />
+             <input 
+            type="password"
+            placeholder="password"
+            />
+            <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Login</button>
+            <h5>
+            <Link to="/signup">Don't have an account ?</Link>
+        </h5>
+            </div>
+        </div>
     )
 }
 
--- client/src/components/Screen/Signup/Signup.jsx
+++ client/src/components/Screen/Signup/Signup.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 const Signup = () => {
     return (
-        <h1>Signup</h1>
+        <div className="mycard">
+        <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input 
+        type="text"
+        placeholder="name"
+        />
+        <input 
+        type="email"
+        placeholder="email"
+        />
+         <input 
+        type="password"
+        placeholder="password"
+        />
+        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Signup</button>
+        <h5>
+            <Link to="/signin">Already have an account ?</Link>
+        </h5>
+
+        </div>
+    </div>
     )
 }
 
