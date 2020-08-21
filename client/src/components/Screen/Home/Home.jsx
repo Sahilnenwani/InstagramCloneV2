7932c3423582dba3e8c40765faf8f9e2ba13e1b6
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-21 14:53:49 +0500

    working with profile.jsx and home.jsx and plus css working both inline and external css working this thing done

--- client/src/components/Screen/Home/Home.jsx
+++ client/src/components/Screen/Home/Home.jsx
@@ -2,7 +2,49 @@ import React from 'react'
 
 const Home=()=>{
     return (
-       <h1>Home</h1>
+       
+       <div className="Home">
+           <div className="card home-card">
+            <h5>sharoz</h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            </div>
+            <div className="card-content">
+            <i className="material-icons" style={{color:"red"}}>favorite</i>
+                <h6>title</h6>
+                <p>This is amazing post </p>
+                <input type="text" placeholder="add a comment" />
+            </div>
+           </div>
+
+           <div className="card home-card">
+            <h5>sharoz</h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            </div>
+            <div className="card-content">
+            <i className="material-icons" style={{color:"red"}}>favorite</i>
+                <h6>title</h6>
+                <p>This is amazing post </p>
+                <input type="text" placeholder="add a comment" />
+            </div>
+           </div>
+
+
+           <div className="card home-card">
+            <h5>sharoz</h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            </div>
+            <div className="card-content">
+                <i className="material-icons" style={{color:"red"}}>favorite</i>
+                <h6>title</h6>
+                <p>This is amazing post </p>
+                <input type="text" placeholder="add a comment" />
+            </div>
+           </div>
+
+       </div>
     )
 }
 
--- client/src/components/Screen/Profile/Profile.jsx
+++ client/src/components/Screen/Profile/Profile.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 const Profile = ()=> {
     return (
-        <h1>Profile</h1>
+       
+        <div style={{
+            maxWidth:"550px",margin:"0px auto"
+        }}>
+            <div style={{
+                display:"flex",
+                justifyContent:"space-around",
+                margin:"18px 0px",
+                borderBottom:"1px solid grey"
+
+            }}>
+                <div>
+                    <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+                   src="https://instagram.fkhi2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/33322403_1511327815661713_7310469237590458368_n.jpg?_nc_ht=instagram.fkhi2-1.fna.fbcdn.net&_nc_ohc=NqfklR4PdMUAX9GV1pQ&oh=6c04544b816dee5133a8f57ab2043808&oe=5F67DCDA"
+                    />
+                </div>
+                <div>
+                    <h4>Sharoz Raees</h4>
+                    <div style={{
+                        display:"flex",
+                        justifyContent:"space-between",
+                        width:'108%'
+                    }}>
+                        <h5>56 posts</h5>
+                        <h5>122 followers</h5>
+                        <h5>465 following</h5>
+                    </div>
+                </div>
+            </div>
+        
+        <div className="gallery">
+                    <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                    <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                    <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                    <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                    <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                    <img className="item" src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+        </div>
+        
+        </div>
     )
 }
 
