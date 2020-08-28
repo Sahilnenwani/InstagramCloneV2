a6892162f359f411017faa83e4e537e4fbb44921
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-28 13:16:29 +0500

    likes and unlikes heart  color change and store value in array

--- client/src/components/Screen/Home/Home.jsx
+++ client/src/components/Screen/Home/Home.jsx
@@ -1,8 +1,11 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 
+import { UserContext } from "../../../App";
 const Home=()=>{
 
 const [data,setDate]= useState([])
+
+const {state,dispatch} = useContext(UserContext)
 
 useEffect(()=>{
         fetch('/allpost',{
@@ -10,8 +13,68 @@ useEffect(()=>{
                 "Authorization":"Bearer "+localStorage.getItem("jwt")
             }
         }).then(res=>res.json())
-        .then(result=>setDate(result.posts))
+        .then(result=>{
+            console.log(result)
+            setDate(result.posts)})
 },[])
+
+const likePost = (id) =>{
+
+    fetch('/like',{
+        method:"put",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":"Bearer "+localStorage.getItem("jwt")
+        },
+        body:JSON.stringify({
+            postId:id
+        })
+    }).then(res=>res.json())
+    .then(result=>{
+        //console.log(result)
+        const newData = data.map(item=>{
+            if (item._id==result._id) {
+                return result
+            }
+            else{
+                return item
+            }
+        })
+            setDate(newData) 
+    
+    
+    }).catch(err=>console.log(err))
+}
+
+
+
+const unlikePost = (id) =>{
+
+    fetch('/unlike',{
+        method:"put",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":"Bearer "+localStorage.getItem("jwt")
+        },
+        body:JSON.stringify({
+            postId:id
+        })
+    }).then(res=>res.json())
+    .then(result=>{
+        
+        //console.log(result)
+
+        const newData = data.map(item=>{
+            if (item._id==result._id) {
+                return result
+            }
+            else{
+                return item
+            }
+        })
+            setDate(newData)       
+    }).catch(err=>console.log(err))
+}
 
 
 
@@ -31,7 +94,21 @@ useEffect(()=>{
                         <img src={item.photo}/>
                     </div>
                     <div className="card-content">
-                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                   {/* <i className="material-icons" style={{color:"red"}}>favorite</i> */}
+                   
+                    {
+                        item.likes.includes(state._id)
+                        ? 
+                         //<i className="material-icons" onClick={()=>unlikePost(item._id)}>thumb_down</i>
+                         <i className="material-icons" onClick={()=>unlikePost(item._id)} style={{color:"red",cursor: "pointer",fontSize:"2.5em"}}>favorite</i>
+                         :
+                         <i className="material-icons" onClick={()=>likePost(item._id)} style={{color:"red",cursor: "pointer",fontSize:"2.5em"}}>favorite_border</i>
+                       // <i className="material-icons" onClick={()=>likePost(item._id)}>thumb_up</i>
+                        
+                    }
+                  
+                    
+                    <h6>{item.likes.length} likes</h6>
                     <h6>{item.title}</h6>
                         <p>{item.body}</p>
                         <input type="text" placeholder="add a comment" />
