3f1723d1c70b1e519d226fbc5cbfa34482983672
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-09-02 18:58:23 +0500

    working with update pic profile.jsx state and upload pic and useeffect logic will be write

--- client/src/components/Screen/Profile/Profile.jsx
+++ client/src/components/Screen/Profile/Profile.jsx
@@ -5,6 +5,8 @@ const Profile = ()=> {
 
     const[mypics,setPics] = useState([])
     const {state,dispatch} = useContext(UserContext)
+    const [image,setImage] = useState("")
+    const [url,setUrl] = useState("")
     // console.log(state)
     useEffect(()=>{
 
@@ -18,23 +20,56 @@ const Profile = ()=> {
 
     },[])
 
+useEffect(()=>{
 
+    if (image) {
+     
+        const data  = new FormData()
+    data.append("file",image)
+    data.append("upload_preset","insta-clone")
+    data.append("cloud_name","LastCode")
+       fetch("https://api.cloudinary.com/v1_1/lastcode/image/upload",{
+           method:"post",
+           body:data
+       })
+       .then(res=>res.json())
+       .then(data=>setUrl(data.url))
+       .catch(err=>console.log(err))
+
+    }
+
+
+
+},[image])
+
+
+const updatePhoto = (file)=>{
+    setImage(file)
+    
+
+}
     return (
        
         <div style={{
             maxWidth:"550px",margin:"0px auto"
         }}>
             <div style={{
-                display:"flex",
-                justifyContent:"space-around",
+              
                 margin:"18px 0px",
                 borderBottom:"1px solid grey"
+
+            }}>
+            <div style={{
+                display:"flex",
+                justifyContent:"space-around",
+              
 
             }}>
                 <div>
                     <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
                    src={state?state.pic:"loading"}
                     />
+                      
                 </div>
                 <div>
                     <h4> {state?state.name:"loading"} </h4>
@@ -52,7 +87,19 @@ const Profile = ()=> {
                     </div>
                 </div>
             </div>
-        
+            
+           
+            <div className="file-field input-field" style={{margin:"10px"}}>
+            <div className="btn #64b5f6 blue darken-1">
+                <span>Update pic</span>
+                <input type="file" onChange={(e)=>updatePhoto(e.target.files[0])} />
+            </div>
+            <div className="file-path-wrapper">
+                <input className="file-path validate" type="text" />
+            </div>
+            </div>
+
+            </div>
         <div className="gallery">
             {
                 mypics.map(item=>{
