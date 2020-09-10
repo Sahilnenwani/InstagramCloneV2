bbcfe0e0e8a95e1851198453bd862d6da65bc130
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-09-10 20:32:41 +0500

    some change for deploy app on heroku

--- server/server.js
+++ server/server.js
@@ -86,6 +86,15 @@ app.get('/',(req,res)=>{
     res.send("Server Is Running!");
 })
 
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+}
+
+
+
+
 app.listen(PORT,()=>{
     console.log(`Server is runnig http://localhost:${PORT}/`)
 })
